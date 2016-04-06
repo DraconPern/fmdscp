e0efcee4d0d1ab352338aeb8df8ab81bb8d55339
DraconPern <draconpern@hotmail.com>
2016-04-05 22:21:28 -0500

    update build

--- build.bat
+++ build.bat
@@ -1,11 +1,7 @@
-IF "%1"=="Debug" (
-  SET TYPE=Debug
+IF "%1"=="Release" (
+SET TYPE=Release
 ) ELSE (
-  IF "%1"=="Release" (
-    SET TYPE=Release
-  ) ELSE (
-    SET TYPE=Debug
-  )
+SET TYPE=Debug
 )
 
 SET BUILD_DIR=%CD%
@@ -13,21 +9,19 @@ SET DEVSPACE=%CD%
 SET CL=/MP
 
 cd %DEVSPACE%
-REM git clone --branch=master https://github.com/madler/zlib.git
-git clone --branch=master http://192.168.1.34/DraconPern/zlib.git
+git clone --branch=master https://github.com/madler/zlib.git
 cd zlib
 git pull
 mkdir build-%TYPE%
 cd build-%TYPE%
-cmake .. -G "Visual Studio 12" -DCMAKE_C_FLAGS_RELEASE="/MT /O2 /D NDEBUG" -DCMAKE_C_FLAGS_DEBUG="/D_DEBUG /MTd /Od" -DCMAKE_INSTALL_PREFIX=%DEVSPACE%\zlib\%TYPE%
+cmake.exe .. -G "Visual Studio 11" -DCMAKE_C_FLAGS_RELEASE="/MT /O2 /D NDEBUG" -DCMAKE_C_FLAGS_DEBUG="/D_DEBUG /MTd /Od" -DCMAKE_INSTALL_PREFIX=%DEVSPACE%\zlib\%TYPE%
 msbuild /P:Configuration=%TYPE% INSTALL.vcxproj
 if ERRORLEVEL 1 exit /B %ERRORLEVEL%
 IF "%TYPE%" == "Release" copy /Y %DEVSPACE%\zlib\Release\lib\zlibstatic.lib %DEVSPACE%\zlib\Release\lib\zlib_o.lib
 IF "%TYPE%" == "Debug"   copy /Y %DEVSPACE%\zlib\Debug\lib\zlibstaticd.lib %DEVSPACE%\zlib\Debug\lib\zlib_d.lib
 
 cd %DEVSPACE%
-REM git clone https://github.com/openssl/openssl.git --branch OpenSSL_1_0_2-stable --single-branch
-git clone http://192.168.1.34/DraconPern/openssl.git --branch OpenSSL_1_0_2-stable --single-branch
+git clone https://github.com/openssl/openssl.git --branch OpenSSL_1_0_2-stable --single-branch
 cd openssl
 SET OLDPATH=%PATH%
 rem SET PATH=C:\Perl\bin;%PATH%
@@ -39,36 +33,53 @@ SET OPENSSL_ROOT_DIR=%DEVSPACE%\openssl\%TYPE%
 SET PATH=%OLDPATH%
 
 cd %DEVSPACE%
-REM git clone git://git.dcmtk.org/dcmtk.git
-git clone http://192.168.1.34/DraconPern/dcmtk.git
+git clone git://git.dcmtk.org/dcmtk.git
 cd dcmtk
 git pull
 git checkout -f 5371e1d84526e7544ab7e70fb47e3cdb4e9231b2
 mkdir build-%TYPE%
 cd build-%TYPE%
-cmake .. -G "Visual Studio 12" -DDCMTK_WIDE_CHAR_FILE_IO_FUNCTIONS=1 -DDCMTK_WITH_ZLIB=1 -DWITH_ZLIBINC=%DEVSPACE%\zlib\%TYPE% -DCMAKE_INSTALL_PREFIX=%DEVSPACE%\dcmtk\%TYPE%
+cmake .. -G "Visual Studio 11" -DDCMTK_WIDE_CHAR_FILE_IO_FUNCTIONS=1 -DDCMTK_WITH_ZLIB=1 -DWITH_ZLIBINC=%DEVSPACE%\zlib\%TYPE% -DCMAKE_INSTALL_PREFIX=%DEVSPACE%\dcmtk\%TYPE%
 msbuild /maxcpucount:8 /P:Configuration=%TYPE% INSTALL.vcxproj
 if ERRORLEVEL 1 exit /B %ERRORLEVEL%
 
 cd %DEVSPACE%
-REM git clone --branch=openjpeg-2.1 --single-branch https://github.com/uclouvain/openjpeg.git
-git clone --branch=openjpeg-2.1 --single-branch http://192.168.1.34/DraconPern/openjpeg.git
+git clone --branch=openjpeg-2.1 https://github.com/uclouvain/openjpeg.git
 cd openjpeg
 git pull
 mkdir build-%TYPE%
 cd build-%TYPE%
-cmake .. -G "Visual Studio 12" -DBUILD_THIRDPARTY=1 -DBUILD_SHARED_LIBS=0 -DCMAKE_C_FLAGS_RELEASE="/MT /O2 /D NDEBUG" -DCMAKE_C_FLAGS_DEBUG="/D_DEBUG /MTd /Od" -DCMAKE_INSTALL_PREFIX=%DEVSPACE%\openjpeg\%TYPE%
+cmake .. -G "Visual Studio 11" -DBUILD_THIRDPARTY=1 -DBUILD_SHARED_LIBS=0 -DCMAKE_C_FLAGS_RELEASE="/MT /O2 /D NDEBUG" -DCMAKE_C_FLAGS_DEBUG="/D_DEBUG /MTd /Od" -DCMAKE_INSTALL_PREFIX=%DEVSPACE%\openjpeg\%TYPE%
 msbuild /P:Configuration=%TYPE% INSTALL.vcxproj
 if ERRORLEVEL 1 exit /B %ERRORLEVEL%
 
 cd %DEVSPACE%
-REM git clone --branch=master https://github.com/DraconPern/fmjpeg2koj.git
-git clone --branch=master http://192.168.1.34/DraconPern/fmjpeg2koj.git
+git clone --branch=master https://github.com/DraconPern/fmjpeg2koj.git
 cd fmjpeg2koj
 git pull
 mkdir build-%TYPE%
 cd build-%TYPE%
-cmake .. -G "Visual Studio 12" -DBUILD_SHARED_LIBS=OFF -DBUILD_THIRDPARTY=ON -DCMAKE_CXX_FLAGS_RELEASE="/MT /O2 /D NDEBUG" -DCMAKE_CXX_FLAGS_DEBUG="/D_DEBUG /MTd /Od" -DOPENJPEG=%DEVSPACE%\openjpeg\%TYPE% -DDCMTK_DIR=%DEVSPACE%\dcmtk\%TYPE% -DCMAKE_INSTALL_PREFIX=%DEVSPACE%\fmjpeg2koj\%TYPE%
+cmake .. -G "Visual Studio 11" -DBUILD_SHARED_LIBS=OFF -DBUILD_THIRDPARTY=ON -DCMAKE_CXX_FLAGS_RELEASE="/MT /O2 /D NDEBUG" -DCMAKE_CXX_FLAGS_DEBUG="/D_DEBUG /MTd /Od" -DOPENJPEG=%DEVSPACE%\openjpeg\%TYPE% -DDCMTK_DIR=%DEVSPACE%\dcmtk\%TYPE% -DCMAKE_INSTALL_PREFIX=%DEVSPACE%\fmjpeg2koj\%TYPE%
+msbuild /P:Configuration=%TYPE% INSTALL.vcxproj
+if ERRORLEVEL 1 exit /B %ERRORLEVEL%
+
+cd %DEVSPACE%
+wget -c https://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.1.zip
+unzip -n jasper-1.900.1.zip
+cd jasper
+mkdir build-%TYPE%
+cd build-%TYPE%
+cmake .. -G "Visual Studio 11" -DBUILD_SHARED_LIBS=0 -DCMAKE_C_FLAGS_RELEASE="/MT /O2 /D NDEBUG" -DCMAKE_C_FLAGS_DEBUG="/D_DEBUG /MTd /Od" -DJASPERDIR=%DEVSPACE%\jasper-1.900.1 -DCMAKE_INSTALL_PREFIX=%DEVSPACE%\jasper\%TYPE%
+msbuild /P:Configuration=%TYPE% INSTALL.vcxproj
+if ERRORLEVEL 1 exit /B %ERRORLEVEL%
+
+cd %DEVSPACE%
+git clone --branch=master https://github.com/DraconPern/fmjpeg2kjasper.git
+cd fmjpeg2kjasper
+git pull
+mkdir build-%TYPE%
+cd build-%TYPE%
+cmake .. -G "Visual Studio 11" -DBUILD_SHARED_LIBS=OFF -DBUILD_THIRDPARTY=ON -DCMAKE_CXX_FLAGS_RELEASE="/MT /O2 /D NDEBUG" -DCMAKE_CXX_FLAGS_DEBUG="/D_DEBUG /MTd /Od" -DJASPER=%DEVSPACE%\jasper\%TYPE% -DDCMTK_DIR=%DEVSPACE%\dcmtk\%TYPE% -DCMAKE_INSTALL_PREFIX=%DEVSPACE%\fmjpeg2kjasper\%TYPE%
 msbuild /P:Configuration=%TYPE% INSTALL.vcxproj
 if ERRORLEVEL 1 exit /B %ERRORLEVEL%
 
@@ -77,10 +88,10 @@ wget -c http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0
 unzip -n boost_1_60_0.zip
 cd boost_1_60_0
 call bootstrap
-SET BOOSTMODULES=--with-atomic --with-thread --with-filesystem --with-system --with-date_time --with-regex --with-context --with-coroutine --with-chrono --with-random
-rem http://lists.boost.org/Archives/boost/2014/08/216440.php
-IF "%TYPE%" == "Release" b2 toolset=msvc-12.0 asmflags=\safeseh runtime-link=static define=_BIND_TO_CURRENT_VCLIBS_VERSION=1 -j 4 %BOOSTMODULES% stage release
-IF "%TYPE%" == "Debug"   b2 toolset=msvc-12.0 asmflags=\safeseh runtime-link=static define=_BIND_TO_CURRENT_VCLIBS_VERSION=1 -j 4 %BOOSTMODULES% stage debug
+SET COMMONb2Flag=toolset=msvc-11.0 runtime-link=static define=_BIND_TO_CURRENT_VCLIBS_VERSION=1 -j 4 stage
+SET BOOSTmodules=--with-atomic --with-thread --with-filesystem --with-system --with-date_time --with-regex --with-context --with-coroutine --with-chrono --with-random
+IF "%TYPE%" == "Release" b2 %COMMONb2Flag% %BOOSTmodules% release
+IF "%TYPE%" == "Debug"   b2 %COMMONb2Flag% %BOOSTmodules% debug
 
 cd %DEVSPACE%
 wget -c https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.6-src.zip
@@ -94,8 +105,7 @@ if ERRORLEVEL 1 exit /B %ERRORLEVEL%
 SET MYSQL_DIR=%DEVSPACE%\mysql-connector-c-6.1.6-src\%TYPE%
 
 cd %DEVSPACE%
-REM git clone https://github.com/awslabs/aws-sdk-cpp.git
-git clone http://192.168.1.34/DraconPern/aws-sdk-cpp.git
+git clone https://github.com/awslabs/aws-sdk-cpp.git
 cd aws-sdk-cpp
 git pull
 mkdir build-%TYPE%
@@ -106,8 +116,7 @@ msbuild /maxcpucount:8 /p:Configuration=%TYPE% INSTALL.vcxproj
 if ERRORLEVEL 1 exit /B %ERRORLEVEL%
 
 cd %DEVSPACE%
-REM git clone https://github.com/pocoproject/poco.git --branch poco-1.6.1 --single-branch
-git clone http://192.168.1.34/DraconPern/poco.git --branch poco-1.6.1 --single-branch
+git clone https://github.com/pocoproject/poco.git --branch poco-1.6.1 --single-branch
 cd poco
 mkdir build-%TYPE%
 cd build-%TYPE%
@@ -117,8 +126,7 @@ if ERRORLEVEL 1 exit /B %ERRORLEVEL%
 
 cd %BUILD_DIR%
 git pull
-REM git clone https://github.com/eidheim/Simple-Web-Server.git
-git clone http://192.168.1.34/DraconPern/Simple-Web-Server.git
+git clone https://github.com/eidheim/Simple-Web-Server.git
 mkdir build-%TYPE%
 cd build-%TYPE%
 cmake .. -G "Visual Studio 12" -DCMAKE_BUILD_TYPE=%TYPE% -DBOOST_ROOT=%DEVSPACE%\boost_1_60_0 -DDCMTK_DIR=%DEVSPACE%\dcmtk\%TYPE% -DZLIB_ROOT=%DEVSPACE%\zlib\%TYPE% -DFMJPEG2K=%DEVSPACE%\fmjpeg2koj\%TYPE% -DOPENJPEG=%DEVSPACE%\openjpeg\%TYPE% -DPOCO=%DEVSPACE%\poco\%TYPE%
